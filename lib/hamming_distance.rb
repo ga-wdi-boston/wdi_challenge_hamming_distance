8df8dc989b83fd9e4d8ae1189a50790b240b00d1
Ashlinn Brennan <asbren13@gmail.com>
2015-01-15 15:06:43 -0500

    challenge complete (finally)

--- lib/hamming_distance.rb
+++ lib/hamming_distance.rb
@@ -2,4 +2,19 @@ class StringsUnequalLength < ArgumentError
 end
 
 def hamming_distance(string_1, string_2)
+  string_one = string_1.scan /\w/
+  string_two = string_2.scan /\w/
+  difference = 0
+  if string_1.length != string_2.length
+    raise StringsUnequalLength
+  end
+  string_one.each do |a|
+    index = 0
+    if a != string_two[index]
+      index +=1
+      difference +=1
+    end
+  end
+  return difference
 end
+
