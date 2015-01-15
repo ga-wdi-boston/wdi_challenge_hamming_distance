4cde8801977b658924b8df4446037b4c1b7f75c5
J.Marx <jennyrutledge@gmail.com>
2015-01-15 09:33:46 -0500

    Add comments to hamm distance function

--- lib/hamming_distance.rb
+++ lib/hamming_distance.rb
@@ -4,10 +4,10 @@ end
 def hamming_distance(string_1, string_2)
   countDiff = 0
   ind = 0
-  if string_1.length == string_2.length
-    if string_1 == string_2
+  if string_1.length == string_2.length #Error
+    if string_1 == string_2 #Checks equal string
       countDiff
-    else
+    else #Count different character.
       arr_1 = string_1.split("")
       arr_2 = string_2.split("")
 
